27b5e059c446a38bd8944c51021d71c86f0623fb
MSK <mshakirmomin@gmail.com>
2026-02-19 03:31:04 +0530

    temporarily

---
+++ backend/import.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+const fs = require("fs");
+
+const connection = mysql.createConnection(process.env.MYSQL_URL);
+
+const sql = fs.readFileSync("smart_farming.sql", "utf8");
+
+connection.query(sql, (err) => {
+  if (err) {
+    console.error("IMPORT ERROR:", err);
+  } else {
+    console.log("DATA IMPORTED SUCCESSFULLY");
+  }
+  connection.end();
+});
