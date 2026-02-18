eca18acffa88e9f55d54ebeb8b00bb94cabd720c
MSK <mshakirmomin@gmail.com>
2026-02-18 18:21:10 +0530

    fix railway port

--- Backend/server.js
+++ Backend/server.js
@@ -353,6 +353,8 @@ app.post("/save-soil", (req, res) => {
 });
 
 // ================== START SERVER ==================
-app.listen(5000, () => {
-  console.log("🚀 Server running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port " + PORT);
 });
